665a7c13b7c90d998b80118060082a1835c2787a
EricWpG <ericwpg2@gmail.com>
2015-09-02 18:48:36 -0500

    Added uncompleted Temp converter

---
+++ 002-Temperature Converter/002-Eric-TempConvert.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TempConvert {
+
+	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter the temperature you would like to convert, followed by the corresponding letter.");
+		System.out.println("Example : 32C");
+	}
+
+}
