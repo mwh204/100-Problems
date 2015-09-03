1c0f5b003fccc433e70104f2ad0bf1f195ce777c
EricWpG <ericwpg2@gmail.com>
2015-09-02 19:27:34 -0500

    Finished temp convert problem

--- 002-Temperature Converter/002-Eric-TempConvert.java
+++ 002-Temperature Converter/002-Eric-TempConvert.java
@@ -5,8 +5,18 @@ public class TempConvert {
 	public static void main(String[] args) {
 		
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Enter the temperature you would like to convert, followed by the corresponding letter.");
-		System.out.println("Example : 32C");
+		System.out.println("Enter the temperature you would like to convert, followed by the corresponding letter.\nExample : 32C");
+		String answer = userInput.nextLine();
+		Double temp = Double.parseDouble(answer.substring(0,answer.length()-1));;
+		
+		if (answer.charAt(answer.length()-1) == 'F'){
+			System.out.println(answer + " = " + ((temp - 32) * 5/9) + "°C");
+		}else if (answer.charAt(answer.length()-1) == 'C'){
+			System.out.println(answer + " = " + (temp * 9/5 + 32) + "°F");
+		}else{
+			System.out.println("Invalid Temperature.");
+		}
+		
 	}
 
 }
