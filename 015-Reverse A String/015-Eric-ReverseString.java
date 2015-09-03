9c5fc91f728195c4d52c3ecf3f5b12f4ed0a253a
EricWpG <ericwpg2@gmail.com>
2015-09-02 20:19:36 -0500

    Fixed it

--- 015-Reverse A String/015-Eric-ReverseString.java
+++ 015-Reverse A String/015-Eric-ReverseString.java
@@ -6,14 +6,17 @@ public class ReverseString {
 		
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Type anything.");
-		String input = userInput.nextLine();
-		String output = ("");
+		System.out.println(reverseString(userInput.nextLine()));
 		
+	}
+	
+	public static String reverseString(String input){
+		String output = ("");
+
 		for (int i=0;i<input.length();i++){
 			output = input.substring(i,i+1) + output;
 		}
-		
-		System.out.println(output);
+		return output;
 	}
 
 }
