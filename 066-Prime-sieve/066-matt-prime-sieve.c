f96484bac79214a5551202d08f2934d2c5f910f2
Matthew <matt@Debian>
2015-09-04 16:35:53 -0500

    small fixes

--- 066-Prime-sieve/066-matt-prime-sieve.c
+++ 066-Prime-sieve/066-matt-prime-sieve.c
@@ -5,10 +5,9 @@
 #define LIMIT 100
 
 uint8_t* makePrimeArray(){
-    unsigned long long int i,j;
-
-	uint8_t* primes = malloc(sizeof(int)*LIMIT);
-    primes[2]=1;
+   	uint64_t i,j;
+	uint8_t* primes = malloc(sizeof(uint8_t)*LIMIT);
+	primes[2]=1;
 	for (i=3; i<LIMIT; i+=2) {
 		primes[i] = 1;
 	}
@@ -24,15 +23,15 @@ uint8_t* makePrimeArray(){
 }
 
 
-int main() {
-    int i, j=1;
-    uint8_t* primes = makePrimeArray();
+int main(void) {
+	uint32_t i, j=1;
+	uint8_t* primes = makePrimeArray();
 	for (i=2; i<LIMIT; i++) {
-        if(primes[i])
-            printf("%dth prime:\t%d\n",j++,i);
-
+        	if(primes[i]){
+            		printf("%dth prime:\t%d\n",j++,i);
+		}
 	}
 
 	free(primes);
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
