b93e331edbc72ed8aaa6dabd9675e5176be8eb4f
matt <matt@Thinkpad>
2015-09-16 17:36:32 -0500

    completely rewrote in a better way

--- 072-ascii-name/072-matt-ascii-words/072-matt-ascii.c
+++ 072-ascii-name/072-matt-ascii-words/072-matt-ascii.c
@@ -3,90 +3,29 @@
 
 #include "name.h"
 
-#define ORIG '#'    //do not change
 #define REPL "_/"
-#define SPACE "  "  //must match number of chars in REPL
 
-void selChar(const char** a, int i){
-    int j;
-    for(j=0;j<WIDTH;j++){
-        a[i][j]==ORIG ? printf(REPL) : fputs(SPACE, stdout);
+void space(void){
+    int i;
+    for(i=0; i<strlen(REPL); i++){
+        putchar(' ');
     }
-    fputs(SPACE, stdout);
+}
+
+char translatePos(char a){
+    if(a >= '0' && a <= '9'){ return a - '0';
+    }else if((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')){return (a - 'a') + 10;
+    }else if(a==' '){ return 36;
+    }else return 37;
+
 }
 
 void printLetter(char a, int i){
     int j;
-    switch(a){
-        case 'a':
-        case 'A': selChar(A, i); break;
-        case 'b':
-        case 'B': selChar(B, i); break;
-        case 'c':
-        case 'C': selChar(C, i); break;
-        case 'd':
-        case 'D': selChar(D, i); break;
-        case 'e':
-        case 'E': selChar(E, i); break;
-        case 'f':
-        case 'F': selChar(F, i); break;
-        case 'g':
-        case 'G': selChar(G, i); break;
-        case 'h':
-        case 'H': selChar(H, i); break;
-        case 'i':
-        case 'I': selChar(I, i); break;
-        case 'j':
-        case 'J': selChar(J, i); break;
-        case 'k':
-        case 'K': selChar(K, i); break;
-        case 'l':
-        case 'L': selChar(L, i); break;
-        case 'm':
-        case 'M': selChar(M, i); break;
-        case 'n':
-        case 'N': selChar(N, i); break;
-        case 'o':
-        case 'O': selChar(O, i); break;
-        case 'p':
-        case 'P': selChar(P, i); break;
-        case 'q':
-        case 'Q': selChar(Q, i); break;
-        case 'r':
-        case 'R': selChar(R, i); break;
-        case 's':
-        case 'S': selChar(S, i); break;
-        case 't':
-        case 'T': selChar(T, i); break;
-        case 'u':
-        case 'U': selChar(U, i); break;
-        case 'v':
-        case 'V': selChar(V, i); break;
-        case 'w':
-        case 'W': selChar(W, i); break;
-        case 'x':
-        case 'X': selChar(X, i); break;
-        case 'y':
-        case 'Y': selChar(Y, i); break;
-        case 'z':
-        case 'Z': selChar(Z, i); break;
-        case '0': selChar(_0, i); break;
-        case '1': selChar(_1, i); break;
-        case '2': selChar(_2, i); break;
-        case '3': selChar(_3, i); break;
-        case '4': selChar(_4, i); break;
-        case '5': selChar(_5, i); break;
-        case '6': selChar(_6, i); break;
-        case '7': selChar(_7, i); break;
-        case '8': selChar(_8, i); break;
-        case '9': selChar(_9, i); break;
-        case ' ':
-            printf("%s ", SPC[i]);
-            break;
-        default:
-            printf("%s " , ERR[i]);
-            break;
+    for(j=0; j<WIDTH; j++){
+        LETTERS[translatePos(a)][i][j] == ORIG ? printf(REPL) : space();
     }
+    space();
 }
 
 void printWord(char* str){
@@ -109,6 +48,4 @@ int main(int argc, char* argv[]){
             printWord(argv[i]);
         }
     }
-
-
 }
