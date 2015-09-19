4b13c4a5e3fa78ad6c096e1fb76f21ab63872d67
Matt <matt@Debian>
2015-09-18 21:38:35 -0500

    probably a bit overkill, but most code can be reused if I do the rogue

---
+++ 035-hunt-the-wumpus/035-matt-wumpus/035-matt-wumpus.c
@@ -0,0 +1,227 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<time.h>
+
+#include "main.h"
+
+int isEmpty(point* pos){
+    return !grid[pos->x][pos->y].object;
+}
+
+int inBounds(point* pos){
+    return  pos->x>=0 && pos->x<GRID_SIZE_X &&
+            pos->y>=0 && pos->y<GRID_SIZE_Y;
+}
+
+point* getEmptyPoint(void){
+    point* p = malloc(sizeof(point));
+    do{
+        p->x = rand() % GRID_SIZE_X;
+        p->y = rand() % GRID_SIZE_Y;
+    }while(!isEmpty(p));
+    return p;
+}
+
+void placeObj(point* p, struct objs a){
+    grid[p->x][p->y].object = a.type;
+}
+
+void initGrid(void){
+    int i,j;
+    for(i=PIT; i<NUM_OBJ_TYPES; i++){
+        for(j=0; j<OBJECTS[i].num; j++){
+            point* p = getEmptyPoint();
+            placeObj(p, OBJECTS[i]);
+            free(p);
+        }
+    }
+
+}
+
+player* initPlayer(void){
+    player *pl = malloc(sizeof(player));
+    pl->arrows = NUM_ARR_PL;
+    pl->pos = getEmptyPoint();
+    placeObj(pl->pos, OBJECTS[PLAYER]);
+    return pl;
+}
+
+wumpus* initWumpus(void){
+    wumpus *w = malloc(sizeof(wumpus));
+    w->pos = getEmptyPoint();
+    placeObj(w->pos, OBJECTS[WUMPUS]);
+    return w;
+}
+
+void wumpusMove(wumpus* w){
+    grid[w->pos->x][w->pos->y].object = OBJECTS[NONE].type;
+    free(w->pos);
+    w->pos = getEmptyPoint();
+    grid[w->pos->x][w->pos->y].object = OBJECTS[WUMPUS].type;
+}
+
+void playerNear(player* pl){
+    int8_t i, x0, y0;
+    for(i=0; i<NUM_DIR; i++){
+        x0 = pl->pos->x + DIRECTIONS[i].x;
+        y0 = pl->pos->y + DIRECTIONS[i].y;
+        point p = {x0, y0};
+        if(inBounds(&p)){
+            switch(grid[x0][y0].object){
+                case WUMPUS: printf("You smell a Wumpus\n"); break;
+                case PIT: printf("You feel a breeze\n"); break;
+                case BAT: printf("You hear flapping\n"); break;
+                default: break;
+            }
+        }
+    }
+}
+
+char* playerMove(player* pl, DIR a, wumpus* w){
+    char* msg = "";
+    int8_t x0 = pl->pos->x + DIRECTIONS[a].x;
+    int8_t y0 = pl->pos->y + DIRECTIONS[a].y;
+    point p = {x0, y0};
+    if(inBounds(&p)){
+        switch(grid[x0][y0].object){
+            case WUMPUS:
+                printf("You have been eaten by the wumpus!\n");
+                playerDie(pl, w);
+                break;
+            case PIT:
+                printf("You fell down a pit!\n");
+                playerDie(pl, w);
+                break;
+            case BAT:
+                msg = "Bats carried you away!\n";
+                grid[pl->pos->x][pl->pos->y].object = OBJECTS[NONE].type;
+                pl->pos = getEmptyPoint();
+                grid[pl->pos->x][pl->pos->y].object = OBJECTS[PLAYER].type;
+                break;
+            case ARROW:
+                msg = "You found an arrow!\n";
+                pl->arrows++;
+            default:
+                grid[pl->pos->x][pl->pos->y].object = OBJECTS[EXPLORED].type;
+                grid[pl->pos->x][pl->pos->y].explored = 1;
+                pl->pos->x = x0;
+                pl->pos->y = y0;
+                grid[pl->pos->x][pl->pos->y].object = OBJECTS[PLAYER].type;
+                break;
+        }
+    }
+    return msg;
+}
+
+char* playerShoot(player* pl, DIR a, wumpus* w){
+    char* msg = "";
+    if(pl->arrows > 0){
+        pl->arrows--;
+        if(grid[pl->pos->x+DIRECTIONS[a].x][pl->pos->y+DIRECTIONS[a].y].object == OBJECTS[WUMPUS].type){
+            printf("Congratulations! you have killed the Wumpus\n");
+            endGame(pl, w);
+        }else{
+            wumpusMove(w);
+            msg = "You missed the Wumpus, and he has moved\n";
+        }
+    }else{
+        msg = "You don't have any arrows left\n";
+    }
+    return msg;
+}
+
+void endGame(player* pl, wumpus* w){
+    free(pl->pos);
+    free(pl);
+    free(w->pos);
+    free(w);
+    exit(EXIT_SUCCESS);
+}
+
+void playerDie(player* pl, wumpus* w){
+    printf("You are dead\n");
+    endGame(pl, w);
+}
+
+void drawGrid(player* pl){
+    int i,j,k;
+    clrscr();
+    for(k=0;k<(2*GRID_SIZE_X)+1;k++){
+        putchar('-');
+    }
+    putchar('\n');
+    for(i=0; i<GRID_SIZE_X; i++){
+        for(j=0;j<GRID_SIZE_Y;j++){
+            putchar('|');
+            putchar(OBJECTS[grid[i][j].object].ch);
+        }
+        putchar('|');
+        putchar('\n');
+        if(i<GRID_SIZE_X+1){
+            for(k=0;k<(2*GRID_SIZE_X)+1;k++){
+                putchar('-');
+            }
+            putchar('\n');
+        }
+    }
+    printf("ARROWS: %d\n", pl->arrows);
+}
+
+int input(void){
+    char c;
+    printf("Enter a direction(wasd), or press f then a direction to fire\n:");
+    c = getchar();
+    while('\n'!=getchar());
+    return c;
+}
+
+char* action(player* pl, wumpus* w, int in){
+    char* msg;
+    switch(in){
+        case KEY_UP:
+            msg = playerMove(pl, UP, w);
+            break;
+        case KEY_DOWN:
+            msg = playerMove(pl, DOWN, w);
+            break;
+        case KEY_LEFT:
+            msg = playerMove(pl, LEFT, w);
+            break;
+        case KEY_RIGHT:
+            msg = playerMove(pl, RIGHT, w);
+            break;
+        case KEY_SHOOT:
+            switch(input()){
+                case KEY_DOWN: msg = playerShoot(pl, DOWN, w); break;
+                case KEY_LEFT: msg = playerShoot(pl, LEFT, w); break;
+                case KEY_RIGHT: msg = playerShoot(pl, RIGHT, w); break;
+                case KEY_UP: msg = playerShoot(pl, UP, w); break;
+                default: break;
+            }
+            break;
+        case KEY_QUIT:
+            endGame(pl, w);
+            break;
+        default: break;
+    }
+    return msg;
+}
+
+void game(void){
+    srand((uint32_t)time(NULL));
+    initGrid();
+    player* pl = initPlayer();
+    wumpus* w = initWumpus();
+    char* msg = "";
+    while(1){
+        drawGrid(pl);
+        printf(msg);
+        playerNear(pl);
+        msg = action(pl, w, input());
+    }
+}
+
+int main(void){
+    game();
+}
---
+++ 035-hunt-the-wumpus/035-matt-wumpus/main.h
@@ -0,0 +1,99 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#define clrscr()    puts ("\e[2J\e[1;1H")
+
+#define GRID_SIZE_X 5
+#define GRID_SIZE_Y 5
+
+#define KEY_QUIT    'q'
+#define KEY_SHOOT   'f'
+#define KEY_UP      'w'
+#define KEY_DOWN    's'
+#define KEY_LEFT    'a'
+#define KEY_RIGHT   'd'
+
+#define NUM_PITS    2
+#define NUM_BATS    1
+#define NUM_ARR_OBJ 4   //number of arrows on the ground
+#define NUM_ARR_PL  5   //initial arrow count
+
+
+#ifdef SEE_ALL
+#define cEMPTY      ' '
+#define cEXPLORED   '+'
+#define cPLAYER     '@'
+#define cWUMPUS     '#'
+#define cPIT        'O'
+#define cBAT        '^'
+#define cARROW      '/'
+#else
+#define cEMPTY      ' '
+#define cEXPLORED   '+'
+#define cPLAYER     '@'
+#define cWUMPUS     cEMPTY
+#define cPIT        cEMPTY
+#define cBAT        cEMPTY
+#define cARROW      cEMPTY
+#endif // SEE_ALL
+typedef enum { NONE, EXPLORED, PLAYER, WUMPUS, PIT, BAT, ARROW, NUM_OBJ_TYPES } OBJ_TYPE;
+
+const struct objs {
+    OBJ_TYPE type;
+    char ch;
+    uint8_t num;
+} OBJECTS[NUM_OBJ_TYPES] = {
+    {NONE, cEMPTY, NULL},
+    {EXPLORED, cEXPLORED, NULL},
+    {PLAYER, cPLAYER, 1},
+    {WUMPUS, cWUMPUS, 1},
+    {PIT, cPIT, NUM_PITS},
+    {BAT, cBAT, NUM_BATS},
+    {ARROW, cARROW, NUM_ARR_OBJ}
+};
+
+typedef struct tile{
+    uint8_t explored;
+    OBJ_TYPE object;
+} tile;
+
+tile grid[GRID_SIZE_X][GRID_SIZE_Y];
+
+typedef struct pt {
+    int8_t x, y;
+} point;
+
+typedef enum { UP, DOWN, LEFT, RIGHT, NUM_DIR } DIR;
+const point DIRECTIONS[NUM_DIR] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+typedef struct player {
+    point* pos;
+    uint8_t arrows;
+} player;
+
+typedef struct wumpus {
+    point* pos;
+} wumpus;
+
+/*Prototypes*/
+int isEmpty(point*);
+int inBounds(point*);
+point* getEmptyPoint(void);
+void placeObj(point*, struct objs);
+void initGrid(void);
+void endGame(player* pl, wumpus* w);
+void drawGrid(player*);
+int input(void);
+char* action(player*, wumpus*, int);
+void game(void);
+
+wumpus* initWumpus(void);
+void wumpusMove(wumpus*);
+
+player* initPlayer(void);
+char* playerMove(player*, DIR, wumpus*);
+char* playerShoot(player*, DIR, wumpus*);
+void playerNear(player*);
+void playerDie(player* pl, wumpus* w);
+
+#endif //_MAIN_H
