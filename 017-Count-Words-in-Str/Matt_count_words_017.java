f2d95f9765bb9ce68982c9cd20edba718f116fca
matt <matt@Thinkpad>
2015-10-02 13:01:55 -0500

    added java version that handles excess spaces

---
+++ 017-Count-Words-in-Str/Matt_count_words_017.java
@@ -0,0 +1,21 @@
+
+public class Matt_count_words_017 {
+  
+  
+  public static void main(String[] args) { 
+    String sent = "The quick brown\t fox   jumps over the lazy     brown doge";
+    System.out.println("Number of spaces in \""+sent+"\": "+ countWords(sent));
+  }
+  
+  public static int countWords(String sentence){
+    int count = 0;
+    sentence = sentence.trim().replaceAll(" +", " "); //remove excess spaces
+    for(int i=0; i<sentence.length(); i++){
+      if(sentence.charAt(i) == ' '){
+        count++;
+      }
+    }
+    return count+1;
+  }
+  
+}
