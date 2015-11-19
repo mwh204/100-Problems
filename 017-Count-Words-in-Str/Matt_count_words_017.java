0dd95f2d1dcbd24735e0517659b36f5f0a83d45c
matt <matt@Thinkpad>
2015-11-19 12:28:11 -0600

    regex fix

--- 017-Count-Words-in-Str/Matt_count_words_017.java
+++ 017-Count-Words-in-Str/Matt_count_words_017.java
@@ -4,12 +4,12 @@ public class Matt_count_words_017 {
   
   public static void main(String[] args) { 
     String sent = "The quick brown\t fox   jumps over the lazy     brown doge";
-    System.out.println("Number of spaces in \""+sent+"\": "+ countWords(sent));
+    System.out.println("Number of words in \""+sent+"\": "+ countWords(sent));
   }
   
   public static int countWords(String sentence){
     int count = 0;
-    sentence = sentence.trim().replaceAll(" +", " "); //remove excess spaces
+    sentence = sentence.trim().replaceAll("\\s+", " "); //remove excess spaces
     for(int i=0; i<sentence.length(); i++){
       if(sentence.charAt(i) == ' '){
         count++;
