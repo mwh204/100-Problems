6edc9174ac8a23ac9ba641b03467e13dbcd2b789
Matt <matt@Debian>
2016-02-29 20:01:49 -0600

    .

--- 024-decimal-to-binary/024-matt-dec-to-bin.c
+++ 024-decimal-to-binary/024-matt-dec-to-bin.c
@@ -6,19 +6,19 @@ void printBin(void* n, const size_t size);
 
 void printBin(void* n, const size_t size){
     size_t i, j;
-    uint8_t byte, *b = (uint8_t*) n;
+    uint8_t byte, *b = (uint8_t* ) n;
 
-    for(i=size; i>0; i--){
-        for(j=CHAR_BIT; j>0; j--){
-            byte = b[i-1] & (1U<<(j-1));
-            printf("%d", (byte >>= (j-1)));
+    for(i=size; i; i--){
+        for(j=CHAR_BIT; j; j--){
+            byte = b[i-1] & (1U << (j-1));
+            printf("%d", byte >>= (j-1));
         }
     }
     putchar('\n');
 }
 
 int main(void){
-    uint16_t a = 5;
+    uint32_t a = 5;
     double b = 13.37;
     uint64_t c = 987654321;
     uint16_t d = 0xF65;
